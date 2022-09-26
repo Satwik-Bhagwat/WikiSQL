02577052c86a9a92cb07c3521dc228115bec625d
Satwik Bhagwat <69520386+Satwik-Bhagwat@users.noreply.github.com>
2022-09-26 15:41:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import load_data
 import load_model
 import train
 
+import torch
+
+device = torch.device("cuda")
+
 td,tt,dd,dt,tl,dl = load_data.get_data('.',2)
 
 rm,t,rc = load_model.get_roberta_model()
